df8bb5f0da175b85acc5082c5619f2724d1d220c
mohammed kaif <mkaif555123@gmail.com>
2025-01-25 16:03:57 +0500

    vercel changings

--- BACK_END/index.mjs
+++ BACK_END/index.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from "cors";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,28 +8,26 @@ app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json());
 
 const products = [
-    { id: 1, name: "Product 1", price: 100 },
-    { id: 2, name: "Product 2", price: 200 },
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
 ];
 
 app.get('/', (request, response) => {
-    response.send("hello world");
+  response.send("Hello World");
 });
 
 app.get('/products', (request, response) => {
-    response.json(products);
+  response.json(products);
 });
 
 app.post('/products', (request, response) => {
-    const newProduct = request.body;
-    products.push(newProduct);
-    response.status(201).json(newProduct);
+  const newProduct = request.body;
+  products.push(newProduct);
+  response.status(201).json(newProduct);
 });
 
 app.use((request, response) => {
-    response.status(404).send("No Route Found");
+  response.status(404).send("No Route Found");
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+export default app;
